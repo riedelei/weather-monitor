13a053cfe5bbd3339afd2183fd3155b640e16931
ChristinaRiedel <christina.riedel@adesso.de>
2024-08-15 14:04:16 +0200

    CityService finished

--- src/main/java/de/riedelei/weather/city/CityService.java
+++ src/main/java/de/riedelei/weather/city/CityService.java
@@ -1,13 +1,22 @@
 package de.riedelei.weather.city;
 
+import de.riedelei.weather.weatherdata.Weather;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 // https://openweathermap.org/api/geocoding-api
 @Service
 public class CityService {
     private String placesUrl = "http://api.openweathermap.org/geo/1.0/direct?q=";
 
-    public City getLonLatForCity(String city) {
+    public City callCityData(String city) {
+        RestTemplate restTemplate = new RestTemplate();
+        setUrlWithLatLon(city);
+        return restTemplate.getForObject(placesUrl, City.class);
 
+    }
+
+    private void setUrlWithLatLon(String city) {
+        this.placesUrl.concat(city).concat("&appid={}");
     }
 }
