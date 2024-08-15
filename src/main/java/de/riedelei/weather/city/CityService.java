3043cad39e01fc787a87522991ea666c3e4976c2
ChristinaRiedel <christina.riedel@adesso.de>
2024-08-15 14:00:30 +0200

    url added for city management

--- src/main/java/de/riedelei/weather/city/CityService.java
+++ src/main/java/de/riedelei/weather/city/CityService.java
@@ -2,6 +2,12 @@ package de.riedelei.weather.city;
 
 import org.springframework.stereotype.Service;
 
+// https://openweathermap.org/api/geocoding-api
 @Service
 public class CityService {
+    private String placesUrl = "http://api.openweathermap.org/geo/1.0/direct?q=";
+
+    public City getLonLatForCity(String city) {
+
+    }
 }
