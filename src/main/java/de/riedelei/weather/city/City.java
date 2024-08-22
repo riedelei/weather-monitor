9f14f5f347a4d7b1c1e61e683f0a09152d5d3591
ChristinaRiedel <christina.riedel@adesso.de>
2024-08-22 11:28:05 +0200

    datenbank erfolgreich hinzugefügt

--- src/main/java/de/riedelei/weather/city/City.java
+++ src/main/java/de/riedelei/weather/city/City.java
@@ -3,12 +3,14 @@ package de.riedelei.weather.city;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table
 @Data
 @Builder
 @NoArgsConstructor
